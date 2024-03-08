107e3c5370ad92ba30592beea58fc7d2b1a5217d
AnnaBea <pereiradelima938@gmail.com>
2024-03-08 13:39:14 -0300

    Projeto criado

---
+++ RestaurantApi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RestaurantApi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
